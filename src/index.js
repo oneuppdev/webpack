8a6c51dd4d45757c66f131d51f041b2f5a8ed18e
Henry Saint-Juste <oneuppdev@gmail.com>
2022-12-03 20:39:01 -0500

    use webpack to generate html that will inject script and styles tags and reference bundle.js and style.css files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 import Heading from "./components/heading/heading";
-// import addImage from "./add-image";
+import addImage from "./add-image";
 
 const heading = new Heading();
 heading.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
-// addImage();
+
+addImage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "none",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    publicPath: "",
   },
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/,
         type: "asset",
-        parser: { dataUrlCondition: { maxSize: 3 * 1024 } },
+        parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
       },
       {
         test: /\.txt/,
@@ -45,7 +47,9 @@ module.exports = {
   plugins: [
     new TerserPlugin(),
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: "styles.[contenthash].css",
     }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
   ],
 };
